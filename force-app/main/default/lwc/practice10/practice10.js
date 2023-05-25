0946893b347c55b53f2b649a8113b5822cf1e3b4
Dildora Ata <Dildorata@gmail.com>
2023-05-25 16:30:37 -0400

    updated code

---
+++ force-app/main/default/lwc/practice10/practice10.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class Practice10 extends LightningElement {
+
+    foodArray = [
+        { id: 1, foodName: 'Pizza' },
+        { id: 2, foodName: 'Burger' },
+        { id: 3, foodName: 'Pasta' },
+        { id: 4, foodName: 'Sushi' },
+        { id: 5, foodName: 'Taco' },
+        { id: 6, foodName: 'Salad' },
+        { id: 7, foodName: 'Sandwich' },
+        { id: 8, foodName: 'Fried Chicken' },
+        { id: 9, foodName: 'Steak' },
+        { id: 10, foodName: 'Lobster' }
+    ];
+
+
+}
